de3a185802f6df870a0e6f5b1a2357da27c6e58d
DuHerbst <du.herbst@gmail.com>
2026-01-08 14:30:10 -0700

    Update script to try to add some movement as well as adding basic sprites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,33 +1,58 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 
 {
+    // FEATURES
+    [SerializeField] private float moveSpeed = 5f;
+    
     private MainActions _testActions; // this _testActions is now the new name of PlayerInputActions
-
+    private Vector2 _moveInput;
+    
     private void Awake()
     {
-        _testActions = new MainActions(); // we have created an object now!! this is the name of the inputs created at first and then we say taht we willc reate a NEW one
-        _testActions.Enable(); // we turn it on to listen to key inputs
-        
+        _testActions = new MainActions(); // create the input actions object
+        Debug.Log("PlayerMovement Awake: MainActions created");
     }
 
     void OnEnable()
-    {
+    { 
+        _testActions = new MainActions();
+        _testActions.Enable(); // enable when the component is enabled
+
         _testActions.Character.Jump.performed += Jump; // when jump is performed, call the Jump function
-        
+        _testActions.Character.Move.performed += OnMove;
+        _testActions.Character.Move.canceled += OnMove;
+
     }
 
     void OnDisable()
     {
-        _testActions.Character.Jump.performed -= Jump;
+            _testActions.Character.Jump.performed -= Jump;
+            _testActions.Character.Move.performed -= OnMove;
+            _testActions.Character.Move.canceled -= OnMove;
+            _testActions.Disable();
+            
+            Debug.Log("PlayerMovement OnDisable: actions disabled");
+        
     }
 
     void Jump(InputAction.CallbackContext ctx)
     {
-        Debug.Log("Jumped!");
+        Debug.Log ("Jumped!");
+    }
+
+    void OnMove(InputAction.CallbackContext ctx)
+    {
+        _moveInput = ctx.ReadValue<Vector2>();
+        Debug.Log("Move Input: " + _moveInput + " | phase: " + ctx.phase);
+    }
+
+    private void Update()
+    {
+        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y);
+        transform.Translate(move * (moveSpeed * Time.fixedDeltaTime));
     }
     
     
