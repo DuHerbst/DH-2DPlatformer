55649fe5873e8b9e9434e093ee5253672b7c8dc6
DuHerbst <du.herbst@gmail.com>
2026-01-08 11:15:42 -0700

    First Jump Action added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+
+{
+    
+    private PlayerInputActions _testActions; // this _testActions is now the new name of PlayerInputActions
+
+    private void Awake()
+    {
+        _testActions = new PlayerInputActions(); // we have created an object now!! this is the name of the inputs created at first and then we say taht we willc reate a NEW one
+        _testActions.Enable(); // we turn it on to listen to key inputs
+        
+    }
+
+    void OnEnable()
+    {
+        _testActions.Player.Jump.performed -= Jump; // when jump is performed, call the Jump function
+        
+    }
+
+    void OnDisable()
+    {
+        _testActions.Player.Jump.performed -= Jump;
+    }
+
+    void Jump(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Jumped!");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
