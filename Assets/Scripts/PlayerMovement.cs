a06afcda5da0d50f82c6125df2163d32e25da02f
DuHerbst <du.herbst@gmail.com>
2026-01-14 16:28:50 -0700

    Update Scripts and input manager

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,53 +5,55 @@ public class PlayerMovement : MonoBehaviour
 
 {
     // FEATURES
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float jumpForce = 1f;
+    [SerializeField] private float accelerationRampUp = 1f;
     
-    private MainActions _testActions; // this _testActions is now the new name of PlayerInputActions
+    //INPUT ACTIONS
+    
+    private MainActions _testActions; // this _testActions is now the new name of PlayerInputActions || underscores are used when the value is private
     private Vector2 _moveInput;
+    public System.Action onJump;
+    public System.Action <float> onMove; // this action needs a value inside <> -- that is why we add float.
+    
     
     private void Awake()
     {
         _testActions = new MainActions(); // create the input actions object
-        Debug.Log("PlayerMovement Awake: MainActions created");
+        _testActions.Enable(); // enable when the component is enabled
+        Debug.Log("Main Actions created");
     }
 
     void OnEnable()
     { 
-        _testActions = new MainActions();
-        _testActions.Enable(); // enable when the component is enabled
-
-        _testActions.Character.Jump.performed += Jump; // when jump is performed, call the Jump function
+        _testActions.Character.Jump.performed += OnJumpPressed; // when jump is performed, call the Jump function
         _testActions.Character.Move.performed += OnMove;
-        _testActions.Character.Move.canceled += OnMove;
 
     }
 
     void OnDisable()
     {
-            _testActions.Character.Jump.performed -= Jump;
+            _testActions.Character.Jump.performed -= OnJumpPressed;
             _testActions.Character.Move.performed -= OnMove;
-            _testActions.Character.Move.canceled -= OnMove;
-            _testActions.Disable();
-            
-            Debug.Log("PlayerMovement OnDisable: actions disabled");
         
     }
 
-    void Jump(InputAction.CallbackContext ctx)
+    void OnJumpPressed(InputAction.CallbackContext ctx)
     {
+        onJump?.Invoke(); // "if the jump has listeners then invoke the jump action - lsiteners are the input system
         Debug.Log ("Jumped!");
     }
 
     void OnMove(InputAction.CallbackContext ctx)
     {
-        _moveInput = ctx.ReadValue<Vector2>();
-        Debug.Log("Move Input: " + _moveInput + " | phase: " + ctx.phase);
+        //_moveInput = ctx.ReadValue<Vector2>();
+        onMove?.Invoke(ctx.ReadValue<float>()); // The value of invoke is taken from the Read Value. Invokes the move action
+        Debug.Log("Move Input: " + ctx.ReadValue<float>());
     }
 
     private void Update()
     {
-        Vector3 move = new Vector3(_moveInput.x, 0, _moveInput.y);
+        Vector3 move = new Vector3(_moveInput.x,_moveInput.y, 0);
         transform.Translate(move * (moveSpeed * Time.fixedDeltaTime));
     }
     
